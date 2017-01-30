d53b70f7d2157af875e84e152906c873af326e2a
Alfred Tchondjo <iriven@yahoo.fr>
2017-01-30 12:50:42 +0100

    Create Url.php

---
+++ Rules/Url.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjhc1170
+ * Date: 18/01/2017
+ * Time: 11:51
+ */
+
+namespace UltimatePHPValidation\Rules;
+
+
+use UltimatePHPValidation\Core\Regex;
+use UltimatePHPValidation\Core\ValidationInterface;
+
+class Url extends Regex implements ValidationInterface
+{
+    private static $CODE = 'NOT_URL';
+    /**
+     * Checks that a value is a valid URL according to http://www.w3.org/Addressing/URL/url-spec.txt
+     *
+     * The regex checks for the following component parts:
+     *
+     * - a valid, optional, scheme
+     * - a valid ip address OR
+     *   a valid domain name as defined by section 2.3.1 of http://www.ietf.org/rfc/rfc1035.txt
+     *   with an optional port number
+     * - an optional valid path
+     * - an optional query string (get parameters)
+     * - an optional fragment (anchor tag)
+     *
+     * @param string $url Value to check
+     * @param bool $strict Require URL to be prefixed by a valid scheme (one of http(s)/ftp(s)/file/news/gopher)
+     * @return bool Success
+     */
+    public static function validate($url, $strict = false)
+    {
+        $hostnamePattern = '(?:[_\p{L}0-9][-_\p{L}0-9]*\.)*(?:[\p{L}0-9][-\p{L}0-9]{0,62})\.(?:(?:[a-z]{2}\.)?[a-z]{2,})';
+        $pattern4 = '(?:(?:25[0-5]|2[0-4][0-9]|(?:(?:1[0-9])?|[1-9]?)[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|(?:(?:1[0-9])?|[1-9]?)[0-9])';
+        $pattern6 = '((([0-9A-Fa-f]{1,4}:){7}(([0-9A-Fa-f]{1,4})|:))|(([0-9A-Fa-f]{1,4}:){6}';
+        $pattern6 .= '(:|((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})';
+        $pattern6 .= '|(:[0-9A-Fa-f]{1,4})))|(([0-9A-Fa-f]{1,4}:){5}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})';
+        $pattern6 .= '(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(([0-9A-Fa-f]{1,4}:)';
+        $pattern6 .= '{4}(:[0-9A-Fa-f]{1,4}){0,1}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2}))';
+        $pattern6 .= '{3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(([0-9A-Fa-f]{1,4}:){3}(:[0-9A-Fa-f]{1,4}){0,2}';
+        $pattern6 .= '((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|';
+        $pattern6 .= '((:[0-9A-Fa-f]{1,4}){1,2})))|(([0-9A-Fa-f]{1,4}:){2}(:[0-9A-Fa-f]{1,4}){0,3}';
+        $pattern6 .= '((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2}))';
+        $pattern6 .= '{3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(([0-9A-Fa-f]{1,4}:)(:[0-9A-Fa-f]{1,4})';
+        $pattern6 .= '{0,4}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)';
+        $pattern6 .= '|((:[0-9A-Fa-f]{1,4}){1,2})))|(:(:[0-9A-Fa-f]{1,4}){0,5}((:((25[0-5]|2[0-4]';
+        $pattern6 .= '\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|((:[0-9A-Fa-f]{1,4})';
+        $pattern6 .= '{1,2})))|(((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})))(%.+)?';
+        $validChars = '([' . preg_quote('!"$&\'()*+,-.@_:;=~[]') . '\/0-9\p{L}\p{N}]|(%[0-9a-f]{2}))';
+        $regex = '/^(?:(?:https?|ftps?|sftp|file|news|gopher):\/\/)' . (!empty($strict) ? '' : '?') .
+            '(?:' . $pattern4 . '|\[' . $pattern6 . '\]|' . $hostnamePattern. ')(?::[1-9][0-9]{0,4})?' .
+            '(?:\/?|\/' . $validChars . '*)?' .
+            '(?:\?' . $validChars . '*)?' .
+            '(?:#' . $validChars . '*)?$/iu';
+        return parent::check($url, $regex);
+    }
+    /**
+     * @return string
+     */
+    public static function getCode(){return self::$CODE;}
+}
