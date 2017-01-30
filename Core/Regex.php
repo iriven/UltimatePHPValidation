08fac9b182d55d5fa4e141ced39b63b4688a5905
Alfred Tchondjo <iriven@yahoo.fr>
2017-01-30 11:34:26 +0100

    Update Regex.php

--- Core/Regex.php
+++ Core/Regex.php
@@ -1,3 +1,28 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Iriven
+ * Date: 08/01/2017
+ * Time: 04:46
+ */
+
 namespace UltimatePHPValidation\Core;
-class Regex{}
+
+/**
+ * Class Regex
+ * @package UltimatePHPValidation\Core
+ */
+abstract class Regex
+{
+    /**
+     * @param $value
+     * @param $pattern
+     * @return bool
+     */
+    public static function check($value, $pattern)
+    {
+        if(is_scalar($value) or !is_string($pattern)) return false;
+        if((preg_match($pattern, $value)!==false))  return true;
+        return false;
+    }
+}
