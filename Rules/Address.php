1ec82c39084c4f68069d4cac362de37570dcbfdf
Alfred Tchondjo <iriven@yahoo.fr>
2017-01-30 11:43:02 +0100

    Update Address.php

--- Rules/Address.php
+++ Rules/Address.php
@@ -1,3 +1,36 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Iriven
+ * Date: 08/01/2017
+ * Time: 07:00
+ */
+
 namespace UltimatePHPValidation\Rules;
-class Address {}
+
+
+use UltimatePHPValidation\Core\Regex;
+use UltimatePHPValidation\Core\ValidationInterface;
+
+/**
+ * Class Address
+ * @package UltimatePHPValidation\Rules
+ */
+class Address extends Regex implements ValidationInterface
+{
+    /**
+     * @param $address
+     * @return bool
+     */
+    public static function validate($address)
+    {
+        $regex='/^[^!<>?=+@{}_$%]*$/ui';
+        if(empty($address)) return true;
+        return parent::check($address,$regex);
+    }
+    private static $CODE = 'INVALID_ADDRESS';
+    /**
+     * @return string
+     */
+    public static function getCode(){return self::$CODE;}
+}
